2ba277ac7d363120aa6f2737b73a349bc110e3ee
mogardo <mogardolof@gmail.com>
2018-10-26 19:39:58 +0200

    black + white body class

--- wp-content/themes/twentyseventeen/template-styles.php
+++ wp-content/themes/twentyseventeen/template-styles.php
@@ -5,5 +5,5 @@ get_header();
 <div class="styles-test">
     <h1>Titel h1</h1>
     <h2>Titel h1</h2>
-    <p>brödtext p</p>
+    <white-body>brödtext Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</white-body>
 </div>
