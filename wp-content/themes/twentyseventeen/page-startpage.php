22853f6c8665df2aff4e4c70d4380ebb296c98a3
Emil <karlperemil@gmail.com>
2018-10-26 18:59:54 +0200

    db

--- wp-content/themes/twentyseventeen/page-startpage.php
+++ wp-content/themes/twentyseventeen/page-startpage.php
@@ -2,7 +2,7 @@
 /* Template Name: Start Page */
 get_header();
 ?>
-<canvas class="glslCanvas" id="glslCanvas" data-fragment-url="/wp-content/themes/twentyseventeen/assets/watercolor.frag" width="500" height="500"></canvas>
+<canvas class="glslCanvas" id="glslCanvas" data-fragment-url="/wp-content/themes/twentyseventeen/assets/watercolor.frag" width="500" height="500" textures="/wp-content/themes/twentyseventeen/assets/om-dvd.png"></canvas>
 <div class="main-title-wrap">
     <div class="main-title">
         <h1>Emil Jönsson</h1>
